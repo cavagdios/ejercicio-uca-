b14edb8a45da7feb90c27b7e7634f2f549ac451f
ejercicio-uca1 <maximolust@gmail.com>
2025-04-03 18:37:26 -0300

    Add files via upload

---
+++ untitled6.py
@@ -0,0 +1,12 @@
+valor1=input("ingrese el primer valor")
+valor2=input("ingrese el segundo valor")
+
+
+if valor1 > valor2:
+  print("el valor1 es mayor")
+
+elif valor2 > valor1:
+  print("el valor2 es mayor")
+
+elif valor1 == valor2:
+    print("valores iguales")
