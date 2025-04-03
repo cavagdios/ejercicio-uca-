8fdee08d8fd32644993e725bd64f5b56b045307b
ejercicio-uca1 <maximolust@gmail.com>
2025-04-03 18:37:52 -0300

    Add files via upload

---
+++ untitled5.py
@@ -0,0 +1,17 @@
+ #90 o + genial
+#80 o + muy bueno 
+#70 o + bueno 
+#- de 70 necesita mejorar 
+#
+
+calificacion = 
+
+if calificacion >= 90:
+    print("excelente")
+elif calificacion >=80:
+    print("muy bueno")
+elif calificacion >= 70:
+    print("bueno")
+elif calificacion <=70:
+    print("necesita mejorar")
+    
